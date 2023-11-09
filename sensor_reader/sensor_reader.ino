9e8c6fbb62210010efd3681f1dfd47e7805a73db
Mouad Fiali <fialimouad@gmail.com>
2023-11-09 11:54:19 +0100

    Added sensor calibration and reading functionality

--- sensor_reader/sensor_reader.ino
+++ sensor_reader/sensor_reader.ino
@@ -1,42 +1,78 @@
-#include "LSM6DS3.h"
-#include "Wire.h"
+// These constants won't change:
+const int sensorPins[] = {A0, A1, A2, A3, A4};  // pins that the sensors are attached to
 
-LSM6DS3 myIMU(I2C_MODE, 0x6A);
-const int capteurPin1 = A0;
-const int capteurPin2 = A1;
-const int capteurPin3 = A2;
-const int capteurPin4 = A3;
-const int capteurPin5 = A4;
+// variables:
+int sensorValues[5] = {0, 0, 0, 0, 0};   // the sensor values
+int sensorMins[5] = {1023, 1023, 1023, 1023, 1023};  // minimum sensor values
+int sensorMaxs[5] = {0, 0, 0, 0, 0};     // maximum sensor values
+
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-  myIMU.begin();
+
+    Serial.begin(9600);
+    Serial.print("Debut de calbiration");
+
+    // calibrate during the first five seconds
+    while (millis() < 20000) {
+        for (int i = 0; i < 5; i++) {
+            sensorValues[i] = analogRead(sensorPins[i]);
+
+            // record the maximum sensor value
+            if (sensorValues[i] > sensorMaxs[i]) {
+                sensorMaxs[i] = sensorValues[i];
+            }
+
+            // record the minimum sensor value
+            if (sensorValues[i] < sensorMins[i]) {
+                sensorMins[i] = sensorValues[i];
+            }
+        }
+    }
+
+     Serial.print("Fin de calbiration");
+
+    for (int i = 0; i < 5; i++) {
+        Serial.print("SenserValue du flex ");
+        Serial.print(i);
+        Serial.print(" : ");
+        Serial.println(sensorMins[i]);
+
+
+        Serial.print("Max du flex ");
+        Serial.print(i);
+        Serial.print(" : ");
+        Serial.println(sensorMaxs[i]);
+
+        Serial.print("Min du flex ");
+        Serial.print(i);
+        Serial.print(" : ");
+        Serial.println(sensorMins[i]);
+    }
+            
+
+
+
+   
 }
 
 void loop() {
-  int valeurCapteur1 = analogRead(capteurPin1);
-  int valeurCapteur2 = analogRead(capteurPin2);
-  int valeurCapteur3 = analogRead(capteurPin3);
-  int valeurCapteur4 = analogRead(capteurPin4);
-  int valeurCapteur5 = analogRead(capteurPin5);
+    for (int i = 0; i < 5; i++) {
+        // read the sensor:
+        sensorValues[i] = analogRead(sensorPins[i]);
 
-  Serial.print(myIMU.readFloatAccelX(), 4);
-  Serial.print(",");
-  Serial.print(myIMU.readFloatAccelY(), 4);
-  Serial.print(",");
-  Serial.print(myIMU.readFloatAccelZ(), 4);
-  Serial.print(",");
-  Serial.print(valeurCapteur1);
-  Serial.print(",");
-  Serial.print(valeurCapteur2);
-  Serial.print(",");
-  Serial.print(valeurCapteur3);
-  Serial.print(",");
-  Serial.print(valeurCapteur4);
-  Serial.print(",");
-  Serial.print(valeurCapteur5);
-  Serial.println();  // End of the line
+        // in case the sensor value is outside the range seen during calibration
+        sensorValues[i] = constrain(sensorValues[i], sensorMins[i], sensorMaxs[i]);
 
-  delay(1000);
+        // apply the calibration to the sensor reading
+        sensorValues[i] = map(sensorValues[i], sensorMins[i], sensorMaxs[i], 0, 255);
+
+        Serial.print("Valeur du capteur de flexion ");
+        Serial.print(i);
+        Serial.print(" : ");
+        Serial.println(sensorValues[i]);
+
+        delay(1000); // Attendre 1 seconde (ajustez selon vos besoins)
+
+
+    }
 }
